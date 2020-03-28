26ad55bf54c68899452404e09ba14fe3a81f8fcc
Prudence Codjia <pcodjia@ironguard.net>
2020-03-28 09:36:21 -0400

    Adding to the Single Page App

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -4,24 +4,36 @@ import Directory from './DirectoryComponent';
 import CampsiteInfo from './CampsiteInfoComponent';
 import { CAMPSITES } from '../shared/campsites';
 import { Switch, Route, Redirect } from 'react-router-dom';
+import Contact from './ContactComponent';
 import Home from './HomeComponent';
+import { COMMENTS } from '../shared/comments';
+import { PARTNERS } from '../shared/partners';
+import { PROMOTIONS } from '../shared/promotions';
 import Header from './HeaderComponent';
 import Footer from './FooterComponent';
 
 class Main extends Component {
+
     constructor(props) {
         super(props);
         this.state = {
             campsites: CAMPSITES,
+            comments: COMMENTS,
+            partners: PARTNERS,
+            promotions: PROMOTIONS
         };
     }
 
 
 render() {
 
-        const HomePage = () => {
+    const HomePage = () => {
             return (
-                <Home />
+                <Home
+                    campsite={this.state.campsites.filter(campsite => campsite.featured)[0]}
+                    promotion={this.state.promotions.filter(promotion => promotion.featured)[0]}
+                    partner={this.state.partners.filter(partner => partner.featured)[0]}
+                />
             );
         }
         return (
@@ -29,6 +41,7 @@ render() {
                  <Header />
                  <Switch >
                     <Route path='/home' component={HomePage} />
+                    <Route exact path='/contactus' component={Contact} />
                     <Route exact path='/directory' render={() => <Directory campsites={this.state.campsites} />} />
                     <Redirect to='/home' />
                </Switch>
